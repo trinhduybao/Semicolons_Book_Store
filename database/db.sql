8d5603efef0a4af149df254d31380f958802f51c
Lưu Đức Bình <binhldps25658@fot.edu.vn>
2024-02-20 21:59:15 +0700

    database

--- database/db.sql
+++ database/db.sql
@@ -1,7 +1,6 @@
 USE [master]
 GO
-
-/****** Object:  Database [Semicolons_Book_Store]    Script Date: 2/20/2024 9:02:03 PM ******/
+/****** Object:  Database [Semicolons_Book_Store]    Script Date: 2/20/2024 9:57:46 PM ******/
 CREATE DATABASE [Semicolons_Book_Store]
  CONTAINMENT = NONE
  ON  PRIMARY 
@@ -10,109 +9,339 @@ CREATE DATABASE [Semicolons_Book_Store]
 ( NAME = N'Semicolons_Book_Store_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.BINHLD\MSSQL\DATA\Semicolons_Book_Store_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
  WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
 GO
-
+ALTER DATABASE [Semicolons_Book_Store] SET COMPATIBILITY_LEVEL = 160
+GO
 IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
 begin
 EXEC [Semicolons_Book_Store].[dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET ANSI_NULL_DEFAULT OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET ANSI_NULLS OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET ANSI_PADDING OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET ANSI_WARNINGS OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET ARITHABORT OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET AUTO_CLOSE ON 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET AUTO_SHRINK OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET AUTO_UPDATE_STATISTICS ON 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET CURSOR_CLOSE_ON_COMMIT OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET CURSOR_DEFAULT  GLOBAL 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET CONCAT_NULL_YIELDS_NULL OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET NUMERIC_ROUNDABORT OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET QUOTED_IDENTIFIER OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET RECURSIVE_TRIGGERS OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET  ENABLE_BROKER 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET DATE_CORRELATION_OPTIMIZATION OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET TRUSTWORTHY OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET ALLOW_SNAPSHOT_ISOLATION OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET PARAMETERIZATION SIMPLE 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET READ_COMMITTED_SNAPSHOT OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET HONOR_BROKER_PRIORITY OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET RECOVERY SIMPLE 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET  MULTI_USER 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET PAGE_VERIFY CHECKSUM  
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET DB_CHAINING OFF 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET TARGET_RECOVERY_TIME = 60 SECONDS 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET DELAYED_DURABILITY = DISABLED 
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET ACCELERATED_DATABASE_RECOVERY = OFF  
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET QUERY_STORE = ON
 GO
-
 ALTER DATABASE [Semicolons_Book_Store] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
 GO
-
+USE [Semicolons_Book_Store]
+GO
+/****** Object:  Table [dbo].[accounts]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[accounts](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[username] [varchar](50) NOT NULL,
+	[password] [varchar](50) NOT NULL,
+	[email] [varchar](100) NOT NULL,
+	[first_name] [nvarchar](100) NULL,
+	[last_name] [nvarchar](100) NULL,
+	[address] [nvarchar](500) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[username] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[authorities]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[authorities](
+	[authority_id] [int] IDENTITY(1,1) NOT NULL,
+	[role_id] [int] NULL,
+	[account_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[authority_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[categories]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[categories](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+	[item_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[favorites]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[favorites](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[account_id] [int] NULL,
+	[product_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[feedbacks]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[feedbacks](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[rate] [int] NOT NULL,
+	[content] [nvarchar](300) NULL,
+	[create_date] [datetime] NULL,
+	[account_id] [int] NULL,
+	[product_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[items]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[items](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[order_details]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[order_details](
+	[order_detail_id] [int] IDENTITY(1,1) NOT NULL,
+	[quantity] [int] NOT NULL,
+	[price] [decimal](10, 2) NOT NULL,
+	[order_id] [int] NULL,
+	[product_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[order_detail_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[orders]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[orders](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[order_date] [datetime] NOT NULL,
+	[total_amount] [decimal](10, 2) NOT NULL,
+	[status] [nvarchar](100) NOT NULL,
+	[address] [nvarchar](500) NOT NULL,
+	[voucher_id] [int] NULL,
+	[account_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[product_images]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[product_images](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[image_url] [varchar](max) NOT NULL,
+	[product_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[products]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[products](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[thumbnail_image] [varchar](max) NOT NULL,
+	[name] [nvarchar](255) NOT NULL,
+	[price] [decimal](10, 2) NOT NULL,
+	[suppllier] [nvarchar](255) NULL,
+	[author] [nvarchar](255) NULL,
+	[published_date] [datetime] NULL,
+	[page_count] [int] NULL,
+	[description] [nvarchar](255) NULL,
+	[quantity] [int] NULL,
+	[brand] [nvarchar](255) NULL,
+	[made_in] [nvarchar](255) NULL,
+	[origin] [nvarchar](255) NULL,
+	[color] [nvarchar](255) NULL,
+	[material] [nvarchar](255) NULL,
+	[category_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[roles]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[roles](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](20) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[vouchers]    Script Date: 2/20/2024 9:57:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[vouchers](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[code] [varchar](10) NOT NULL,
+	[discount_amount] [decimal](10, 2) NOT NULL,
+	[condition] [decimal](10, 2) NOT NULL,
+	[valid_form] [datetime] NOT NULL,
+	[valid_to] [datetime] NOT NULL,
+	[create_date] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[authorities]  WITH CHECK ADD FOREIGN KEY([account_id])
+REFERENCES [dbo].[accounts] ([id])
+GO
+ALTER TABLE [dbo].[authorities]  WITH CHECK ADD FOREIGN KEY([role_id])
+REFERENCES [dbo].[roles] ([id])
+GO
+ALTER TABLE [dbo].[categories]  WITH CHECK ADD FOREIGN KEY([item_id])
+REFERENCES [dbo].[items] ([id])
+GO
+ALTER TABLE [dbo].[favorites]  WITH CHECK ADD FOREIGN KEY([account_id])
+REFERENCES [dbo].[accounts] ([id])
+GO
+ALTER TABLE [dbo].[favorites]  WITH CHECK ADD FOREIGN KEY([product_id])
+REFERENCES [dbo].[products] ([id])
+GO
+ALTER TABLE [dbo].[feedbacks]  WITH CHECK ADD FOREIGN KEY([account_id])
+REFERENCES [dbo].[accounts] ([id])
+GO
+ALTER TABLE [dbo].[feedbacks]  WITH CHECK ADD FOREIGN KEY([product_id])
+REFERENCES [dbo].[products] ([id])
+GO
+ALTER TABLE [dbo].[order_details]  WITH CHECK ADD FOREIGN KEY([order_id])
+REFERENCES [dbo].[orders] ([id])
+GO
+ALTER TABLE [dbo].[order_details]  WITH CHECK ADD FOREIGN KEY([product_id])
+REFERENCES [dbo].[products] ([id])
+GO
+ALTER TABLE [dbo].[orders]  WITH CHECK ADD FOREIGN KEY([account_id])
+REFERENCES [dbo].[accounts] ([id])
+GO
+ALTER TABLE [dbo].[orders]  WITH CHECK ADD FOREIGN KEY([voucher_id])
+REFERENCES [dbo].[vouchers] ([id])
+GO
+ALTER TABLE [dbo].[product_images]  WITH CHECK ADD FOREIGN KEY([product_id])
+REFERENCES [dbo].[products] ([id])
+GO
+ALTER TABLE [dbo].[products]  WITH CHECK ADD FOREIGN KEY([category_id])
+REFERENCES [dbo].[categories] ([id])
+GO
+USE [master]
+GO
 ALTER DATABASE [Semicolons_Book_Store] SET  READ_WRITE 
 GO
-
