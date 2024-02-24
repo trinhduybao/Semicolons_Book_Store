5dbc08bc658dd7a415922b0b692587b079eea21e
Tran Van Nhan <ads.tranvannhan@gmail.com>
2024-02-24 10:52:17 +0700

    Insert Service Category And CategoryServiceImpl

---
+++ src/main/java/com/semicolons_book_store/service/CategoryService.java
@@ -0,0 +1,17 @@
+package com.semicolons_book_store.service;
+
+import com.semicolons_book_store.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<Category> getAll();
+
+    Category getOne(Integer id);
+
+    Category save(Category category);
+
+    Category update(Category category);
+
+    void delete(Integer id);
+}
